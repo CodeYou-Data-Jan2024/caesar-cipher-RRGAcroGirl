fa2b4efbed5a60f57e3e7b25279e49e415ebfc3b
Nicole Meyer <meyer.k.nicole@gmail.com>
2024-04-03 18:26:39 -0400

    Completing the Caesar Cipher Assignment

--- cipher.py
+++ cipher.py
@@ -1,1 +1,8 @@
-# add your code here
+sentence = input("Please enter a sentence:")
+sentence = sentence.lower()
+cipher = {'a': 'f', 'b': 'g', 'c': 'h', 'd': 'i', 'e': 'j', 'f': 'k', 'g': 'l', 'h': 'm', 'i': 'n', 'j': 'o', 'k': 'p', 'l': 'q', 'm': 'r', 'n': 's', 'o': 't', 'p': 'u', 'q': 'v', 'r': 'w', 's': 'x', 't': 'y', 'u': 'z', 'v': 'a', 'w': 'b', 'x': 'c', 'y': 'd', 'z': 'e'}
+ans = ""
+for l in sentence:
+    ans += cipher.get(l,l)
+
+print(ans)
